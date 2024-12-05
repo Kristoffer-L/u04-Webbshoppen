c2d4a5773e4553310cc55e34522fc43b241bb86a
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2024-12-05 11:26:54 +0100

    feature: eventlistener

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const mensClothingBtn = document.getElementById("mensClothing")
+const womensClothingBtn = document.getElementById("womensClothing")
+const jewelerybtn = document.getElementById("jewelery")
+const electronicsBtn = document.getElementById("electronics")
+
 async function getInfo() {
     try {
         const response = await fetch("./json/data.json")
@@ -15,8 +20,8 @@ async function getInfo() {
 getInfo()
 
 function renderHTML(data) {
+    
     data.map(({image, title, price, rating:{rate}}) => {
-        
         const card = document.createElement("div")
         document.querySelector(".section").appendChild(card)
         card.classList.add("card-container")
@@ -29,6 +34,21 @@ function renderHTML(data) {
                 <p>Rating: ${rate}/5.0</p>
                 <button>Buy</button>
                 `
-                
+
     })
+    mensClothingBtn.addEventListener("click", () => {
+        console.log(data)
+        const mensClothing = data.filter((v) => {
+            return v.category === "men's clothing";
+        }) 
+        renderHTML(mensClothing);
+    });
 }
+
+
+
+
+
+
+
+
