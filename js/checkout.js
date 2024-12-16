c600aa639bca75996444c133d442b00ac5719be3
FilipHans <filip.hansen98@gmail.com>
2024-12-16 10:43:54 +0100

    added cart is now displayed on checkout page

--- js/checkout.js
+++ js/checkout.js
@@ -1,13 +1,23 @@
-const cart = [{ id: 3, title: "Mens Cotton Jacket", price: 55.99, image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg" }];
+const cart = JSON.parse(localStorage.all);
+const articleCart = document.querySelector(".article-cart");
+const uniqueItems = [...new Map(cart.map(v => [v.id, v])).values()];
 
-const cartItem = document.getElementById("cartItem");
-
-cart.map(({ id, image, title, price }) => {
-  cartItem.innerHTML = `
+uniqueItems.map(({ id, image, title, price }) => {
+  const card = document.createElement("div");
+  const itemTracker = localStorage.getItem(`${id}`);
+  articleCart.appendChild(card);
+  card.classList.add("cart-item");
+  card.innerHTML = `
             <img class="cart-item-img" src="${image}" alt="cart item" />
             <div>
               <h2>${title}</h2>
               <p>price: ${price}</p>
-              <p>antal:</p>
+              <p>antal: ${itemTracker}</p>
             </div>`;
 });
+
+const prices = cart.map((v) => {return v.price});
+
+console.log(prices);
+const total = prices.reduce((acc, cur) => {return acc + cur});
+console.log(total.toFixed(2));
